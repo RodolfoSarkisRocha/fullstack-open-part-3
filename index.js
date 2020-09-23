1a9f7f7ba7d88a53bafdef41545695ad20872eff
Rodolfo Sarkis Rocha <rodolfosarkis.r@gmail.com>
2020-09-23 17:31:34 -0300

    exercises 3.21 - 3.22

--- index.js
+++ index.js
@@ -1,93 +1,93 @@
-const express = require("express");
-const app = express();
-const morgan = require("morgan");
-const { token } = require("morgan");
-const cors = require("cors");
-require("dotenv").config();
+const express = require('express')
+const app = express()
+const morgan = require('morgan')
+const { token } = require('morgan')
+const cors = require('cors')
+require('dotenv').config()
 
-const Person = require("./models/people");
+const Person = require('./models/people')
 
-app.use(cors());
-app.use(express.json());
+app.use(cors())
+app.use(express.json())
 
 // Using build folder to serve frontend app
-app.use(express.static("build"));
+app.use(express.static('build'))
 
 // Creating custom token to log request body to morgan console
-token("body", (req, res) => JSON.stringify(req.body));
+token('body', (req) => JSON.stringify(req.body))
 app.use(
-  morgan(":method :url :status :res[content-length] - :response-time ms :body")
-);
+  morgan(':method :url :status :res[content-length] - :response-time ms :body')
+)
 
 // Get all people from phonebook
-app.get("/api/people", (request, response) => {
+app.get('/api/people', (request, response) => {
   Person.find({})
     .then((people) => response.json(people))
     .catch((err) => {
       response.status(400).json({
         error: err,
-        message: "could not fetch data from the server",
-      });
-    });
-});
+        message: 'could not fetch data from the server',
+      })
+    })
+})
 
 // Information about how many people phonebook has registered and the current time
-app.get("/info", (request, response) => {
+app.get('/info', (request, response) => {
   Person.find({})
     .then((people) => {
-      const totalPeople = people.length;
-      const date = new Date();
+      const totalPeople = people.length
+      const date = new Date()
       response.send(
         `<div>${`Phonebook has info for ${totalPeople} people`}</div><br><div>${date}</div>`
-      );
+      )
     })
     .catch((err) => {
       response.status(400).json({
         error: err,
-        message: "could not fetch data from server",
-      });
-    });
-});
+        message: 'could not fetch data from server',
+      })
+    })
+})
 
 // Get Person by ID
-app.get("/api/people/:id", (request, response, next) => {
-  const id = request.params.id;
+app.get('/api/people/:id', (request, response, next) => {
+  const id = request.params.id
   Person.findById(id)
     .then((note) => {
-      if (note) response.json(note);
-      else response.status(404).end();
+      if (note) response.json(note)
+      else response.status(404).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // Edit Person
 
-app.put("/api/people/:id", (request, response, next) => {
+app.put('/api/people/:id', (request, response, next) => {
   const {
     body: { name, number },
     params: { id },
-  } = request;
+  } = request
 
   const person = {
     name,
     number,
-  };
+  }
 
   // Options for moongose update
-  const opts = { runValidators: true, new: true };
+  const opts = { runValidators: true, new: true }
   Person.findByIdAndUpdate(id, person, opts).then((updatedPerson) =>
     response.json(updatedPerson).catch((error) => next(error))
-  );
-});
+  )
+})
 
 // Delete Person
-app.delete("/api/people/:id", (request, response, next) => {
-  const id = request.params.id;
+app.delete('/api/people/:id', (request, response, next) => {
+  const id = request.params.id
 
   Person.findByIdAndRemove(id)
-    .then((result) => response.status(204).end())
-    .catch((error) => next(error));
-});
+    .then(() => response.status(204).end())
+    .catch((error) => next(error))
+})
 
 // Generates a random id for the POST body
 // const generateId = () => {
@@ -99,13 +99,13 @@ app.delete("/api/people/:id", (request, response, next) => {
 
 // Validates the body of the people POST
 const validateBody = (body, checkValues) => {
-  if (!body) return false;
-  const values = checkValues.map((key) => body[key]);
+  if (!body) return false
+  const values = checkValues.map((key) => body[key])
   const invalidValues = values.some(
-    (value) => value === null || value === undefined || value === ""
-  );
-  return invalidValues;
-};
+    (value) => value === null || value === undefined || value === ''
+  )
+  return invalidValues
+}
 
 // const verifyIfNameExists = (name) => {
 //   return new Promise((resolve, reject) => {
@@ -119,45 +119,45 @@ const validateBody = (body, checkValues) => {
 // };
 
 // people POST
-app.post("/api/people", (request, response, next) => {
-  const { body } = request;
+app.post('/api/people', (request, response, next) => {
+  const { body } = request
 
-  const checkValues = ["name", "number"];
+  const checkValues = ['name', 'number']
   if (validateBody(body, checkValues)) {
     return response.status(400).json({
-      error: "number or name is missing",
-    });
+      error: 'number or name is missing',
+    })
   }
 
-  const { name, number } = body;
+  const { name, number } = body
 
   const person = new Person({
     name,
     number,
-  });
+  })
 
   person
     .save()
     .then((savedPerson) => response.json(savedPerson))
-    .catch((err) => next(err));
-});
+    .catch((err) => next(err))
+})
 
 // Error Handlers
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name === "ValidationError") {
-    return response.status(400).send({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+  console.log(`Server is running on port ${PORT}`)
+})
